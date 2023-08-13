fcf3eaeff614f730a50534021d94d67464290774
Vikash Ramaswamy <83834214+vikappmetry@users.noreply.github.com>
2023-08-13 15:04:52 +0530

    Highscore implementation
    • Removed the unnecessary dependencies from the app.
    • Added the feature to store High Score

--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,34 +8,46 @@ public class LogicScript : MonoBehaviour
 
     public int playerScore;
     public Text scoreText;
+    public Text highScoreText;
     public Text gameOverText;
     public Button restartButton;
-
     public float incrementSpeedAndSpawnBy = 5;
 
+    private int currentHighScore;
 
-    public PipeMoveScript pipe;
-    public PipeSpawnerPrefab pipeSpawn;
-
-
-    private void Start()
+    public void Start()
     {
-        pipeSpawn = GameObject.FindGameObjectWithTag("Finish").GetComponent<PipeSpawnerPrefab>();
-        pipe = GameObject.FindGameObjectWithTag("Pipe").GetComponent<PipeMoveScript>();
-        
-        //Debug.Log(pipeSpawn.spawnRate);
+        currentHighScore = PlayerPrefs.GetInt("HighScore", 0);
+        Debug.Log(currentHighScore);
+        updateHighScore(currentHighScore);
     }
 
-    [ContextMenu("Increase Score")]
+    //[ContextMenu("Increase Score")]
     public void incrementScore(int scoreToAdd) { 
         playerScore+=scoreToAdd;
         scoreText.text = playerScore.ToString();
+        checkHighScore(playerScore);
+        
     }
 
     public void gameOver()
     {
         gameOverText.gameObject.SetActive(true);
         restartButton.gameObject.SetActive(true);
+        if (playerScore >= currentHighScore) {
+            PlayerPrefs.SetInt("HighScore", playerScore);
+        }
+    }
+
+    public void checkHighScore(int playerScore) {
+
+        if (playerScore > currentHighScore) {
+            updateHighScore(playerScore);
+        }   
+    }
+
+    public void updateHighScore(int scoreToUpdate) {
+        highScoreText.text = $"High Score: {scoreToUpdate}";
     }
 
 }
